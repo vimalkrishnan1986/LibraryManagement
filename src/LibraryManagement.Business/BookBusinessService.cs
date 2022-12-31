5c5d58af292350df6aac2fad2b21c90bcda65c68
vimalkrishnan <c-vimal.krishnan@coca-cola.com.ph>
2022-12-31 23:46:19 +0530

    Mapping is added

--- src/LibraryManagement.Business/BookBusinessService.cs
+++ src/LibraryManagement.Business/BookBusinessService.cs
@@ -1,23 +1,50 @@
-﻿using LibraryManagement.Business.Contracts;
+﻿using AutoMapper;
+using LibraryManagement.Business.Contracts;
+using LibraryManagement.Data.Models;
+using LibraryManagement.Data.Repositories;
 using LibraryManagement.Domain.Models;
+using Microsoft.Extensions.Logging;
 
 namespace LibraryManagement.Business
 {
     public class BookBusinessService : IBookBusinessService
     {
-        public Task<BookDomainModel> AddAsync(BookDomainModel bookDomainModel)
+        private readonly IRepository<BookDataModel> _bookRepository;
+        private readonly ILogger _logger;
+        private readonly IMapper _mapper;
+
+        public BookBusinessService(ILogger logger, IRepository<BookDataModel> bookRepsitory, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _bookRepository = bookRepsitory ?? throw new ArgumentNullException(nameof(bookRepsitory));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+        public async Task<BookDomainModel> AddAsync(BookDomainModel bookDomainModel)
+        {
+            var dataModel = _mapper.Map<BookDataModel>(bookDomainModel);
+            await _bookRepository.InsertAsync(dataModel).ConfigureAwait(false);
+            return _mapper.Map<BookDomainModel>(dataModel);
         }
 
-        public Task<IEnumerable<BookDomainModel>> GetAllAsync()
+        public async Task<IEnumerable<BookDomainModel>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var items = await _bookRepository.GetAllAsync().ConfigureAwait(false);
+            if (items == null || !items.Any())
+            {
+                return Enumerable.Empty<BookDomainModel>();
+            }
+            return items.Select(x => _mapper.Map<BookDomainModel>(x));
         }
 
-        public Task<BookDomainModel> GetByIdAsync(Guid resourceId)
+        public async Task<BookDomainModel> GetByIdAsync(Guid resourceId)
         {
-            throw new NotImplementedException();
+            var model = await _bookRepository
+                .GetByFilterAsync(m => m.ResourceId == resourceId).ConfigureAwait(false);
+            if (model == null)
+            {
+                throw new ArgumentException("Invalid resourceId");
+            }
+            return _mapper.Map<BookDomainModel>(model);
         }
     }
 }
